d62f97b5a298a62d3f8c9a8a94fcb4f6bc609e02
SanderGeuens <sander.geuens@student.hogent.be>
2023-12-04 00:18:27 +0100

    verwijderen overbodige commentaar

--- app/src/main/java/com/example/myapplication/network/CoinApiService.kt
+++ app/src/main/java/com/example/myapplication/network/CoinApiService.kt
@@ -8,27 +8,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
-/*
-private const val BASE_URL =
-    //"https://android-kotlin-fun-mars-server.appspot.com"
-    //"https://api.coinlore.net/api/tickers/"
-    "https://api.coincap.io/v2/"
-
-private val logger = HttpLoggingInterceptor().apply{level = HttpLoggingInterceptor.Level.BASIC}
-
-private val client = OkHttpClient.Builder()
-    .addInterceptor(logger)
-    .build()
-
-val retrofit = Retrofit.Builder()
-    .addConverterFactory(
-        /*ScalarsConverterFactory.create()*/
-    Json.asConverterFactory("application/json".toMediaType())
-    )
-    .baseUrl(BASE_URL)
-    .client(client)
-    .build()
-    */
 
 
 interface CoinApiService {
@@ -36,9 +15,3 @@ interface CoinApiService {
     suspend fun getCoins():CoinsResponse
 }
 
-/*
-object CoinApi {
-    val retrofitService : CoinApiService by lazy {
-        retrofit.create(CoinApiService::class.java)
-    }
-}*/
