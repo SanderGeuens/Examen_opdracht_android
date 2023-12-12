459e3afdb664bcd8af1aedd569c0fcea6e10316b
SanderGeuens <sander.geuens@student.hogent.be>
2023-12-12 23:21:44 +0100

    aanpassing top app bar

--- app/src/main/java/com/example/myapplication/AppBar.kt
+++ app/src/main/java/com/example/myapplication/AppBar.kt
@@ -10,9 +10,11 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -31,6 +33,10 @@ fun AppBar (
 
     Surface(shadowElevation = 10.dp) {
         TopAppBar(
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
             title = {
 
                 Text(text="Cryptocoin advise app")
